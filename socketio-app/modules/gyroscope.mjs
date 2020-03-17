0288a40a9baa86c7cbc63db326f081f661d4823d
Fabien Jeckelmann <fabien.jeckelmann@gmail.com>
2020-03-17 13:49:33 +0100

    fix gyroscope promise

--- socketio-app/modules/gyroscope.mjs
+++ socketio-app/modules/gyroscope.mjs
@@ -1,18 +1,20 @@
-export default new Promise((resolve, reject) => {
-  if (!window.DeviceMotionEvent) {
-    reject('DeviceMotionEvent is not supported by device');
-  } else if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    return DeviceMotionEvent.requestPermission()
-      .then((permissionState) => {
-        if (permissionState === 'granted') {
-          resolve('Permission granted');
-        } else {
-          reject(new Error('Permission denied'));
-        }
-      }).catch((error) => {
-        reject(error);
-      });
-  } else {
-    resolve('No Permission needed');
-  }
-});
+export default () => {
+  return new Promise((resolve, reject) => {
+    if (!window.DeviceMotionEvent) {
+      reject('DeviceMotionEvent is not supported by device');
+    } else if (typeof DeviceMotionEvent.requestPermission === 'function') {
+      return DeviceMotionEvent.requestPermission()
+        .then((permissionState) => {
+          if (permissionState === 'granted') {
+            resolve('Permission granted');
+          } else {
+            reject(new Error('Permission denied'));
+          }
+        }).catch((error) => {
+          reject(error);
+        });
+    } else {
+      resolve('No permission needed');
+    }
+  });
+};
